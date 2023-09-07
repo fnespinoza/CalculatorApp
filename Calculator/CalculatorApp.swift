220609dd1ef593bc60c1299e4d461f5b4c36d928
Alberto Orrantia <ricco.albert@gmail.com>
2023-09-07 01:19:49 -0700

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Alberto Orrantia on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
